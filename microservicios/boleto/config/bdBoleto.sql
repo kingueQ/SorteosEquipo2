c419d660ac2bc092f0d2ef5eb61b765410830b82
Abraham Quintana <abraham.quintana233514@potros.itson.edu.mx>
2024-11-11 16:40:42 -0700

    Correcciones menores

--- microservicios/boleto/config/bdBoleto.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS boletos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    idSorteo INT NOT NULL,
-    id_cliente INT,
-    numero INT NOT NULL,
-    estado ENUM('disponible', 'apartado', 'vendido') DEFAULT 'disponible',
-    fechaApartado TIMESTAMP NULL,
-    fechaVenta TIMESTAMP NULL
-);
